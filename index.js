ed455b5efb6feb7fb0f1d47a4c3963d34254a6da
elondevelop <65175481+elondevelop@users.noreply.github.com>
2020-05-16 08:53:40 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on("ready", () => {
+    client.user.setActivity('the Biggs Server!', { url: 'https://www.twitch.tv/monstercat', type: 'WATCHING' })
+  });
+
+// HANDLER
+
+fs = require("fs");
+//Command Handler
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.events = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+    if (err) return console.log(err);
+    files.forEach(file => {
+        if (!file.endsWith(".js")) return;
+        let props = require(`./commands/${file}`);
+        console.log("Successfully loaded " + file)
+        let commandName = file.split(".")[0];
+        client.commands.set(commandName, props);
+    });
+});
+    //Events "handler"
+    fs.readdir('./events/', (err, files) => {
+        if (err) console.log(err);
+        files.forEach(file => {
+            let eventFunc = require(`./events/${file}`);
+            console.log("Successfully loaded " + file)
+            let eventName = file.split(".")[0];
+            client.on(eventName, (...args) => eventFunc.run(client, ...args));
+        });
+});
+
+// HANDLER
+
+
+client.login('NzExMDMyNjU3MjM2MDY2MzA0.Xr9HGA.hagoD3OQhP7-0Y3ZkPlLHCM8HME');
