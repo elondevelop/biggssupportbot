5590df5ef0bac27c9e7b2e9bca9c33a20eea3136
elondevelop <65175481+elondevelop@users.noreply.github.com>
2020-05-16 08:50:40 -0500

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const helpEmbed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Commands')
+	.setDescription('All of the current commands for BSB')
+	.addFields(
+		{ name: '.ping', value: 'Test Command'},
+	)
+	.addFields(
+		{name : '.kick | .ban ', value: 'Instantly kicks/bans any member in the server if user is an Admin [.kick | .ban @MEMBER]' }
+	)
+	.setFooter('Report any bugs found on the support server (.inv)')
+    .setTimestamp()
+
+exports.run = (client, message, args) => {
+    message.channel.send(helpEmbed)
+}
